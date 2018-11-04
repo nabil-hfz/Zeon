841d6c6a95fba1f38c0b37f91b8d74202509e35a
nabil-hfz <44735062+nabil-hfz@users.noreply.github.com>
2018-11-04 19:36:22 +0200

    update

--- app/src/main/java/com/example/volley/zeon/MenuActivity/FutureVisionActivity.java
+++ app/src/main/java/com/example/volley/zeon/MenuActivity/FutureVisionActivity.java
@@ -24,38 +24,18 @@ public class FutureVisionActivity extends AppCompatActivity {
     /**
      * const Article  for displying in Future Vision UI .
      */
-    public static final String THE_FIRST_ARTICL = "The team plans to expand and reach\n" +
-            "high levels of competition and strong\n" +
-            "on wide ranges, including the Arab\n" +
-            "Mashreq and then the world, through\n" +
-            "organized effort and fine work.";
+    public static final String THE_FIRST_ARTICLE = "The team plans to expand and reach high levels of competition and strong on wide ranges, including the Arab Mashreq and then the world, through organized effort and fine work.";
 
 
     /**
      * const Article  for displying in Future Vision UI .
      */
-    public static final String THE_SECOND_ARTICL = "To succeed in the labor market, making\n" +
-            "the effort to stand by the mountain of\n" +
-            "talented youth and able to develop\n" +
-            "itself with an unrivaled ambition is an\n" +
-            "essential goal of the team by helping\n" +
-            "them find the right opportunities and\n" +
-            "receive the appropriate expertise and\n" +
-            "competencies to promote a strong\n" +
-            "society through training events and\n" +
-            "courses.And development in various\n" +
-            "areas of work.";
+    public static final String THE_SECOND_ARTICLE = "To succeed in the labor market, making the effort to stand by the mountain of talented youth and able to develop itself with an unrivaled ambition is an essential goal of the team by helping them find the right opportunities and receive the appropriate expertise and competencies to promote a strong society through training events and courses.And development in various areas of work.";
 
     /**
      * const Article  for displying in Future Vision UI .
      */
-    public static final String THE_THIRD_ARTICL = "The scientific research team gives an\n" +
-            "important aspect of its future vision as\n" +
-            "it seeks to develop the tools and means\n" +
-            "of work and find all that facilitates the\n" +
-            "work of individuals working in the field\n" +
-            "of software to ensure the durability\n" +
-            "and effectiveness";
+    public static final String THE_THIRD_ARTICLE = "The scientific research team gives an important aspect of its future vision as it seeks to develop the tools and means of work and find all that facilitates the work of individuals working in the field of software to ensure the durability and effectiveness";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,11 +44,11 @@ public class FutureVisionActivity extends AppCompatActivity {
 
         List<FutureVision> futureVisionList = new ArrayList<FutureVision>();
 
-        futureVisionList.add(new FutureVision(THE_FIRST_ARTICL, R.drawable.future_vision));
+        futureVisionList.add(new FutureVision(THE_FIRST_ARTICLE, R.drawable.future_vision));
 
-        futureVisionList.add(new FutureVision(THE_SECOND_ARTICL, R.drawable.future_vision));
+        futureVisionList.add(new FutureVision(THE_SECOND_ARTICLE, R.drawable.future_vision));
 
-        futureVisionList.add(new FutureVision(THE_THIRD_ARTICL, R.drawable.future_vision));
+        futureVisionList.add(new FutureVision(THE_THIRD_ARTICLE, R.drawable.future_vision));
 
         AdapterFutureVision adapter = new AdapterFutureVision(this, futureVisionList);
 
