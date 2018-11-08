acd907ef894fd6ea4289e90a9de3ed8acedfa5a7
nabil-hfz <44735062+nabil-hfz@users.noreply.github.com>
2018-11-08 06:22:33 +0200

    final update

--- app/src/main/java/com/example/volley/zeon/MenuActivity/ContactActivity.java
+++ app/src/main/java/com/example/volley/zeon/MenuActivity/ContactActivity.java
@@ -23,6 +23,7 @@ import com.example.volley.zeon.Util.UtilTools;
  * Author of this class is Louay .
  * <p>
  * Update this class Nabil in in 2018/03/11
+ *
  */
 
 public class ContactActivity extends AppCompatActivity {
