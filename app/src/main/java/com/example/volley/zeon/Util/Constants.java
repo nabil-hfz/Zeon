69237c3198b35334549463c08279e52514f46b2c
nabil-hfz <44735062+nabil-hfz@users.noreply.github.com>
2018-11-23 11:24:59 +0200

    merge our job

---
+++ app/src/main/java/com/example/volley/zeon/Util/Constants.java
@@ -0,0 +1,25 @@
+package com.example.volley.zeon.Util;
+
+public class Constants {
+    //future vision url
+    public static final String FUTURE_VISION_URL = "https://mtmt2141.000webhostapp.com/getFutureVision.php";
+
+    //division url
+    public static final String DIVISION_URL = "https://mtmt2141.000webhostapp.com/getDepartments.php";
+
+    /**
+     * TEAM EMAIL for send the  messages to it .
+     */
+    public static final String T_EMAIL = "https://www.facebook.com/profile.php?id=100003729378979";
+
+    public static final String TEAM_INFO_IMAGE[] = {"https://i.ytimg.com/vi/ktlQrO2Sifg/maxresdefault.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd-mJ0J7sGeROVjPCDOmjAHAaYt6rmjMN1m7pB5Qkju4su0TQZ",
+            "http://mygreatminds.com/wp-content/uploads/2018/04/495272006.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcX4ISMPQ2PF2fP_2gBS82CnzCDsxC91Tt-U-sLn0wnlkavAhK",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThoylveidNpbN5tquegR0LuRI3NdTzpnNoIsrKZN7RFda4C6Aj",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM9OJpWvAUZ0VRvxUA7sh73R-3Rj8JbVuEXmkouNc0dofeF9gqMw",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLVTeDihyHppw9sVlYf90nvG0Tj8oD5HGAuT8DuhcwWNoVjyPi",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAmZP4_87e5ODbM8X6URG686A7wa14jA87TZe8kh5LiPeOmcwe",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTPgDfPHz5ZLJmPM0p6OhUhaLathpJEdl3Xitbe9Mk4WMiKO-mA"};
+}
+
