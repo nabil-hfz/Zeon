5e00be0d399c13daee93843226dca0953335c409
nabil-hfz <44735062+nabil-hfz@users.noreply.github.com>
2018-11-26 23:45:55 +0200

    some job about us act

--- app/src/main/java/com/example/volley/zeon/MainActivity.java
+++ app/src/main/java/com/example/volley/zeon/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.volley.zeon;
 
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -11,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
